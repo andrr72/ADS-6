e86affb495ce08cf527ab9091372b44c1b9bc75b
Andrei <140070906+andrr72@users.noreply.github.com>
2025-05-04 18:18:50 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,58 +2,60 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <stdexcept>
+
 template<typename T>
 class TPQueue {
-private:
+  private:
     struct Node {
-        T data;
-        Node* next;
-        Node(const T& d) : data(d), next(nullptr) {}
+      T data;
+      Node* next;
+      Node(const T& d) : data(d), next(nullptr) {}
     };
     Node* head;
 
-public:
+  public:
     TPQueue() : head(nullptr) {}
 
     ~TPQueue() {
-        while (head) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
+      while (head) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+      }
     }
 
     void push(const T& item) {
-        Node* newNode = new Node(item);
-        if (!head || head->data.prior < item.prior) {
-            newNode->next = head;
-            head = newNode;
-        } else {
-            Node* current = head;
-            while (current->next && current->next->data.prior >= item.prior) {
-                current = current->next;
-            }
-            newNode->next = current->next;
-            current->next = newNode;
+      Node* newNode = new Node(item);
+      if (!head || head->data.prior < item.prior) {
+        newNode->next = head;
+        head = newNode;
+      } else {
+        Node* current = head;
+        while (current->next && current->next->data.prior >= item.prior) {
+          current = current->next;
         }
+        newNode->next = current->next;
+        current->next = newNode;
+      }
     }
 
     T pop() {
-        if (!head) throw std::runtime_error("Queue is empty");
-        Node* temp = head;
-        T data = temp->data;
-        head = head->next;
-        delete temp;
-        return data;
+      if (!head) throw std::runtime_error("Queue is empty");
+      Node* temp = head;
+      T data = temp->data;
+      head = head->next;
+      delete temp;
+      return data;
     }
 
     T& top() {
-        if (!head) throw std::runtime_error("Queue is empty");
-        return head->data;
+      if (!head) throw std::runtime_error("Queue is empty");
+      return head->data;
     }
 
     bool empty() const {
-        return head == nullptr;
+      return head == nullptr;
     }
 };
 
