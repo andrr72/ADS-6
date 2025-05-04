21f2df4b23b9faccd0460e0b1cd6e5c958e89ba3
Andrei <140070906+andrr72@users.noreply.github.com>
2025-05-04 18:14:17 +0300

    Update tpqueue.h dada

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,57 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& d) : data(d), next(nullptr) {}
+    };
+    Node* head;
+
+public:
+    TPQueue() : head(nullptr) {}
+
+    ~TPQueue() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void push(const T& item) {
+        Node* newNode = new Node(item);
+        if (!head || head->data.prior < item.prior) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next && current->next->data.prior >= item.prior) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+
+    T pop() {
+        if (!head) throw std::runtime_error("Queue is empty");
+        Node* temp = head;
+        T data = temp->data;
+        head = head->next;
+        delete temp;
+        return data;
+    }
+
+    T& top() {
+        if (!head) throw std::runtime_error("Queue is empty");
+        return head->data;
+    }
+
+    bool empty() const {
+        return head == nullptr;
+    }
 };
 
 struct SYM {
